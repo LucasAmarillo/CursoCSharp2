8073be07156fb8333d25a87e6899fd62ee226777
unknown <lucas27alf@gmail.com>
2022-05-22 21:42:46 -0300

    primer version

---
+++ Desafio1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la marca del telefono:");
+            string marca = Console.ReadLine();
+            Console.WriteLine("Ingrese el modelo del telefono:");
+            string modelo = Console.ReadLine();
+            Telefono t1 = new Telefono(marca, modelo);
+
+            Console.WriteLine("Ingrese el número de telefono: ");
+            string telefono = Console.ReadLine();
+            t1.NumeroTelefonico = telefono;
+
+            Console.WriteLine("Ingrese el código de operador: ");
+            int codigo = int.Parse(Console.ReadLine());
+            t1.CodigoOperador = codigo;
+
+            Console.WriteLine("Ingrese el contacto: ");
+            string contacto = Console.ReadLine();
+
+            Console.WriteLine("Marca: " + t1.Marca.ToUpper());
+            Console.WriteLine("Modelo: " + t1.Modelo.ToUpper());
+            Console.WriteLine("Número Telefónico " + t1.NumeroTelefonico);
+            Console.WriteLine("Código Operador " + t1.CodigoOperador);
+            Console.WriteLine(t1.llamar());
+            Console.WriteLine(t1.llamar(contacto.ToUpper()));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Desafio1/Telefono.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1
+{
+    class Telefono
+    {
+        public Telefono(string marca, string modelo)
+        {
+            this.marca = marca;
+            this.modelo = modelo;
+        }
+        private string modelo;
+        private string marca;
+        private int codigoOperador; // 1, 2 o 3
+
+        public string Modelo
+        {
+            get { return modelo; }
+        }
+        public string Marca
+        {
+            get { return marca; }
+        }
+        public string NumeroTelefonico { get; set; }
+        public int CodigoOperador
+        {
+            get { return codigoOperador; }
+            set
+            {
+                if (value > 1 && value < 4)
+                {
+                    codigoOperador = value;
+                }
+                else
+                {
+                    codigoOperador = 0;
+                }
+            }
+        }
+        public string llamar()
+        {
+            return "Realizando la llamada...";
+        }
+        public string llamar(string contacto)
+        {
+            return "Llamando a " + contacto;
+        }
+    }
+}
